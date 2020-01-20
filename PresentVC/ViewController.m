45895c73cf4477a02e4d156f81da1f18345b811b
fc19901016 <574993357@qq.com>
2020-01-20 10:48:40 +0800

    更新

--- PresentVC/ViewController.m
+++ PresentVC/ViewController.m
@@ -5,6 +5,9 @@
 //  Created by 冯攀 on 2019/12/20.
 //  Copyright © 2019 冯攀. All rights reserved.
 //
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define SCREEN_SPEACING 10
 
 #import "ViewController.h"
 #import "UIViewController+XPModal.h"
@@ -18,22 +21,82 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSArray *titleArray = @[@"中心", @"上弹", @"右弹", @"下弹", @"左弹"];
+    
+
+    float W = (SCREEN_WIDTH-SCREEN_SPEACING)/3-SCREEN_SPEACING;
+    float H = 50.0;
+
+    for (int i = 0; i < titleArray.count; i++) {
+        float X = SCREEN_SPEACING+i%3*(W+SCREEN_SPEACING);
+        float Y = 100 + floorf(i/3.0)*(H+SCREEN_SPEACING);
+        
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [self.view addSubview:btn];
+        btn.tag = i;
+        btn.frame = CGRectMake(X, Y, W, H);
+        [btn setTitle:titleArray[i] forState:UIControlStateNormal];
+        [btn setTitle:titleArray[i] forState:UIControlStateHighlighted];
+        [btn setTitleColor:UIColor.blackColor forState:UIControlStateNormal];
+        [btn setTitleColor:UIColor.lightGrayColor forState:UIControlStateHighlighted];
+        [btn setBackgroundImage:[self setNavColor:UIColor.orangeColor] forState:UIControlStateNormal];
+        [btn setBackgroundImage:[self setNavColor:UIColor.brownColor] forState:UIControlStateHighlighted];
+
+        [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    }
+}
+- (UIImage *)setNavColor:(UIColor *)color{
+    //位图大小
+    CGSize size = CGSizeMake(1, 1);
+    //绘制位图
+    UIGraphicsBeginImageContext(size);
+    //获取当前创建的内容
+    CGContextRef contextRef = UIGraphicsGetCurrentContext();
+    //充满指定的填充色
+    CGContextSetFillColorWithColor(contextRef, color.CGColor);
+    //充满指定的矩形
+    CGContextFillRect(contextRef, CGRectMake(0, 0, 1, 1));
+    //绘制image
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    //结束绘制/Users/ZCY/Desktop/NavigationDiv
+    UIGraphicsEndImageContext();
+    return image;
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+-(void)btnClick:(UIButton *)btn{
     XPModalConfiguration *configuration = [XPModalConfiguration defaultConfiguration];
-    configuration.direction = XPModalDirectionRight;
-    configuration.enableInteractiveTransitioning = NO;
-    configuration.autoDismissModal = YES;
-    CGSize contentSize = CGSizeMake(200.0, CGFLOAT_MAX);
-    
+    CGSize contentSize = CGSizeMake(0.0, 0.0);
+    if (btn.tag == 0) {
+        configuration.direction = XPModalDirectionCenter;
+        configuration.enableInteractiveTransitioning = NO;
+        configuration.autoDismissModal = YES;
+        contentSize = CGSizeMake(0.7*SCREEN_WIDTH, 0.7*SCREEN_HEIGHT);
+    }else if (btn.tag == 1){
+        configuration.direction = XPModalDirectionTop;
+        configuration.enableInteractiveTransitioning = NO;
+        configuration.autoDismissModal = YES;
+        contentSize = CGSizeMake(SCREEN_WIDTH, 300.0);
+    }else if (btn.tag == 2){
+        configuration.direction = XPModalDirectionRight;
+        configuration.enableInteractiveTransitioning = NO;
+        configuration.autoDismissModal = YES;
+        contentSize = CGSizeMake(200.0, CGFLOAT_MAX);
+    }else if (btn.tag == 3){
+        configuration.direction = XPModalDirectionBottom;
+        configuration.enableInteractiveTransitioning = NO;
+        configuration.autoDismissModal = YES;
+        contentSize = CGSizeMake(SCREEN_WIDTH, 300.0);
+    }else if (btn.tag == 4){
+        configuration.direction = XPModalDirectionLeft;
+        configuration.enableInteractiveTransitioning = NO;
+        configuration.autoDismissModal = YES;
+        contentSize = CGSizeMake(200.0, CGFLOAT_MAX);
+
+    }
+
     ViewRightController *vc = [[ViewRightController alloc] init];
-    
-    //    [self presentModalWithView:vc.view contentSize:contentSize configuration:configuration completion:^{
-    //
-    //    }];
     [self presentModalWithViewController:vc contentSize:contentSize configuration:configuration completion:nil];
+
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # iOS-PresentVC
 上、左、下、右、中 弹出控制器
+
+
+
+
+# 展示图
+
+![image](https://github.com/fc19901016/iOS-PresentVC/blob/master/show.gif )
+
